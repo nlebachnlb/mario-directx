daad1b4ce1a4cfa3d782865f854088d04b60ceeb
Bach Nguyen <nlebachnlb.develop@gmail.com>
2020-11-25 23:44:03 +0700

    Adjust Piranha's parameter

--- SampleFramework/DirectGame/WindowsProject1/PiranhaPlant.h
+++ SampleFramework/DirectGame/WindowsProject1/PiranhaPlant.h
@@ -2,7 +2,7 @@
 #include "Plant.h"
 
 const int PIRANHA_WAIT_TIME = 800;
-const Vector2 PIRANHA_BBOX(16 * 3, 23 * 3);
+const Vector2 PIRANHA_BBOX(16 * 3, 24 * 3);
 const float PIRANHA_SPEED = 0.4f;
 const float PIRANHA_HIDE_DISTANCE = 48;
 
