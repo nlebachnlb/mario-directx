3fce67bea1c3a20cbbccad044c622de7c02e9b52
Bach Nguyen <nlebachnlb.develop@gmail.com>
2020-12-06 11:31:43 +0700

    Add HUD mask

--- SampleFramework/DirectGame/WindowsProject1/MainCanvas.cpp
+++ SampleFramework/DirectGame/WindowsProject1/MainCanvas.cpp
@@ -1,13 +1,23 @@
 #include "MainCanvas.h"
+#include "Game.h"
+#include "Consts.h"
 
 void MainCanvas::Awake()
 {
 	hud = new HudPanel();
 	hud->Initialize();
 	AddUIElement(hud);
+
+	mask = Game::GetInstance().GetService<TextureManager>()->GetTexture(TEXTURE_BOX);
 }
 
 void MainCanvas::Start()
 {
 
+}
+
+void MainCanvas::Render()
+{
+	Game::GetInstance().DrawTexture(0, 594, 0, 0, mask, 0, 0, 824, 150);
+	Canvas::Render();
 }
